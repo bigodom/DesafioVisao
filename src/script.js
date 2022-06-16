bd7dd5e434de71ce7c57fd617f0f177ea1294584
bigodom <pedro_augusto50@hotmail.com>
2022-06-16 14:00:14 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -34,40 +34,40 @@ pedido.addEventListener('click', () => iniciaModal('modal-cadastro'));
 // e mostrar a proxima imagem
 // a imagem atal começa no 0, clicando no avançar adiciona +1
 
-const imagensPainel = document.querySelectorAll('.imagem-painel')
-const setaAvancar = document.getElementById('btn-avancar')
-const setaVoltar = document.getElementById('btn-voltar')
-let imagemAtual = 0
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
 
 setaAvancar.addEventListener('click',function(){
   if(imagemAtual === imagensPainel.length -1){
-    imagemAtual = -1
+    imagemAtual = -1;
   }
 
-  imagemAtual++
+  imagemAtual++;
 
   // removendo o mostrar da imagem:
   imagensPainel.forEach(imagem =>{
-    imagem.classList.remove('mostrar')
+    imagem.classList.remove('mostrar');
   })
 
   // mostrar a proxima imagem:
-  imagensPainel[imagemAtual].classList.add('mostrar')
+  imagensPainel[imagemAtual].classList.add('mostrar');
 })
 
 setaVoltar.addEventListener('click', function(){
 
   if(imagemAtual === 0){
-      imagemAtual = 3
+      imagemAtual = 3;
   }
 
-  imagemAtual--
+  imagemAtual--;
   
   // removendo o mostrar da imagem:
   imagensPainel.forEach(imagem =>{
-      imagem.classList.remove('mostrar')
+      imagem.classList.remove('mostrar');
   })
 
   // mostrar a proxima imagem:
-  imagensPainel[imagemAtual].classList.add('mostrar')
+  imagensPainel[imagemAtual].classList.add('mostrar');
 })
